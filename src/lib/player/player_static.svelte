07b7a6f85d32366a2c446238b57af503bc9e0d6f
Josue <josue.sa.trabajos@gmail.com>
2025-04-11 17:19:34 -0300

    Add controllable video time bar.

--- src/lib/player/player_static.svelte
+++ src/lib/player/player_static.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type * as Model from "$lib/models/index.js";
+  import { effect_once } from "$lib/utils/state.svelte.js";
   import { use_player_ctx } from "./player.svelte.js";
   import ControlsStatic from "./player_controls_static.svelte";
   import Tracklist from "./player_tracklist.svelte";
@@ -32,15 +33,19 @@
   });
 
   let title = $derived(state.single ? "" : state.playlist!.title);
-  let curr_video = $derived(state.single ? state.video : player_state.curr_entry?.video);
+  let curr_video = $derived(state.single ? state.video : player_state.current.video);
 
-  $effect(() => {
+  effect_once(() => {
     if (state.single) {
-      player_state.play(state.video.id);
+      if (player_state.current.video?.id !== state.video.id) {
+        player_state.play(state.video.id);
+      }
     } else {
-      player_state.set_playlist(state.playlist!);
-      player_state.set_entries(state.entries!);
-      player_state.play_by_index(state.start_index);
+      if (player_state.playlist?.id !== state.playlist.id) {
+        player_state.set_playlist(state.playlist);
+        player_state.set_entries(state.entries);
+        player_state.play_by_index(state.start_index);
+      }
     }
   });
 </script>
@@ -49,24 +54,37 @@
   <div class="flex flex-col w-full bg-none">
     <div class="flex-none flex justify-center py-2">
       <h2 class="text-2xl mt-2 font-bold">
-        {title}
+        {#if state.single}
+          {title}
+        {:else}
+          <a href="/playlist/{state.playlist.id}">
+            {state.playlist.title}
+          </a>
+        {/if}
       </h2>
     </div>
     <div class="flex-1 grid place-items-center">
-      {#if player_state.curr_entry_is_unavailable}
+      {#if player_state.current.unavailable}
         <div class="text-red-500 font-bold">Not available</div>
       {:else if curr_video}
         <div class="flex flex-col items-center max-w-[min(80%,36rem)]">
-          <div class="aspect-video rounded-md overflow-hidden">
+          <svg width="0" height="0">
+            <filter id="blur-and-scale" y="-50%" x="-50%" width="200%" height="200%">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="30" result="blurred"></feGaussianBlur>
+              <feColorMatrix type="saturate" in="blurred" values="0.8" result="saturated_blur"></feColorMatrix>
+              <feComposite in="SourceGraphic" in2="saturated_blur" operator="over"></feComposite>
+            </filter>
+          </svg>
+          <div class="aspect-video rounded-md overflow-clip" style="filter: url(#blur-and-scale);">
             <img
               src={curr_video.img?.url}
               height={curr_video.img?.height}
               width={curr_video.img?.width}
               alt="{curr_video.title} video thumbnail"
-              class="object-cover size-full bg-transparent"
+              class="object-cover h-full bg-transparent"
             />
           </div>
-          <div class="text-center my-6">
+          <div class="text-center mt-8">
             <h3 class="text-xl font-bold text-pretty leading-tight">{curr_video.title}</h3>
             <a
               href="/{curr_video.channel_id}"
@@ -89,7 +107,7 @@
     <Tracklist
       playlist={state.playlist}
       entries={player_state.tracks}
-      current_entry={player_state.curr_entry?.item.id}
+      current_entry={player_state.current.entry?.item.id}
       on_select={(_, i) => {
         player_state.play_by_index(i);
       }}
