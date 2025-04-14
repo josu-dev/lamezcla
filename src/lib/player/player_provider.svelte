f6995f5f982e9ae4df83930e76badeb19cb833aa
Josue <josue.sa.trabajos@gmail.com>
2025-04-14 17:33:27 -0300

    Fix persist config overrade.

--- src/lib/player/player_provider.svelte
+++ src/lib/player/player_provider.svelte
@@ -4,20 +4,20 @@
 
   let { children, audio_only = false, options }: PlayerProviderProps = $props();
 
-  const default_target_id = $props.id();
+  const default_target_id = "global-iframe-player-target";
 
   const render_iframe = options?.target_id === undefined;
-  const palyer_opts: PlayerStateOptionsInit = {
+  const player_opts: PlayerStateOptionsInit = {
     target_id: default_target_id,
     height: 64,
     width: 64,
     ...options,
   };
 
-  use_player_ctx(palyer_opts);
+  use_player_ctx(player_opts);
 </script>
 
 {#if render_iframe}
-  <div class={audio_only ? "sr-only invisible" : ""}><div id={palyer_opts.target_id}></div></div>
+  <div class={audio_only ? "sr-only invisible" : ""}><div id={player_opts.target_id}></div></div>
 {/if}
 {@render children()}
